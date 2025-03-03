9711ebce9823a107cd4679a0d9a3d7dcb9667c8f
Henrique Florencio <henrique.florencio307@gmail.com>
2025-03-02 22:01:53 -0300

    fix: logo fix

--- src/utils/logo.tsx
+++ src/utils/logo.tsx
@@ -4,7 +4,7 @@ export function Logo(props: IconInterface) {
   const redTone = "text-red-500";
   return (
     <span className={`select-none font-serif text-base-content italic ${props.className}`}>
-      <span className={redTone}>H</span>enri<span className={redTone}>D</span>ev
+      <span className={redTone}>H</span>ennri<span className={redTone}>D</span>ev
     </span>
   );
 }
